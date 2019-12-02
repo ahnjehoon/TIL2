d5add47f3a91c67d90a349507f4d07148eebc85b
ahnjehoon <ahnjehoon@naver.com>
2019-12-02 22:31:01 +0900

    접속주소 파일 읽는것으로 대체

--- C#/rest_api_server/rest_api_test/Startup.cs
+++ C#/rest_api_server/rest_api_test/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using rest_api_test.Models;
+using System.IO;
 
 namespace rest_api_test
 {
@@ -22,8 +23,13 @@ namespace rest_api_test
         {
             services.AddControllers();
 
-            services.AddDbContext<MSSQLContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MSSQL")));
+            // db세팅 파일
+            string DBConnectionString;
+            // 로컬일때
+            DBConnectionString = Configuration.GetConnectionString("MSSQL");
+            // 개발서버
+            DBConnectionString = File.ReadAllText(@"C:\DBConnection.txt");
+            services.AddDbContext<MSSQLContext>(options => options.UseSqlServer(DBConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
